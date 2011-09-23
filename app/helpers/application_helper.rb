601e4150628803171359438ab60d973bc31dec77
Sunny Ripert <sunny@sunfox.org>
2011-09-23 16:55:30 +0200

    Use actionview number helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,7 @@
 # encoding: utf-8
 module ApplicationHelper
-  def money(amount, currency = "€")
-    amount = amount.to_f.round(2)
-    amount = amount.to_s
-    amount.gsub! /\.0$/, ''
-    amount.gsub! /\..$/, '\00'
-    amount += " #{currency}" unless currency.nil?
-    amount
-  end
-
   def ratio(amount)
-    percent = (amount * 100).to_s
-    percent.gsub! /\.0$/, ''
-    "#{percent} %"
+    number_to_percentage(amount*100, :precision => 0)
   end
 
   def variation(amount)
