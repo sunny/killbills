69a982645f6dafaa668f444855942653a45aaaf9
Sunny Ripert <sunny@sunfox.org>
2012-07-17 22:55:34 +0200

    Refactorisation of action_view_extensions.rb

--- config/initializers/action_view_extensions.rb
+++ config/initializers/action_view_extensions.rb
@@ -1,28 +1,20 @@
 # encoding: utf-8
-module ActionView
-  module Helpers
-    class FormBuilder
-      def number_field(attribute, options={})
-        defaults = {
-          :size => 5,
-          :type => "number"
-        }
-        text_field(attribute, defaults.merge(options))
-      end
+class ActionView::Helpers::FormBuilder
+  def number_field(attribute, options={})
+    defaults = { size: 5, type: "number" }
+    text_field(attribute, defaults.merge(options))
+  end
 
-      def money_field(attribute, options={})
-        defaults = {
-          :min => 0,
-          :step => 0.01,
-          :'data-currency' => "€"
-        }
-        options = defaults.merge(options)
+  def money_field(attribute, options={})
+    defaults = { min: 0, step: 0.01, data: { currency: "€" } }
+    options = defaults.merge(options)
 
-        currency = options[:'data-currency'] ? " <span class=\"currency\">#{options[:'data-currency']}</span>" : ''
-
-        number_field(attribute, options) + currency.html_safe
-      end
+    currency = ""
+    if options[:data][:currency]
+      currency = content_tag(:span, options[:data][:currency], class: "currency")
     end
+
+    number_field(attribute, options) + currency
   end
 end
 
