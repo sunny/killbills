5b0aa6580b028b4fa7d6b82b5e6316c0735724e2
Sunny Ripert <sunny@sunfox.org>
2012-07-27 13:39:14 +0200

    Better comment on AR errors_on_group

--- config/initializers/active_record_extensions.rb
+++ config/initializers/active_record_extensions.rb
@@ -1,5 +1,8 @@
 class ActiveRecord::Base
-  # Helper to add errors on children
+  # Helper to add errors on nested resources
+  #
+  # Example:
+  #   user.errors_on_group(:articles, :title, user.articles, "must not contain foobar")
   def errors_on_group(group, attribute, children, message = "")
     errors[:"#{group}.#{attribute}"] = message
     children.each { |child| child.errors[attribute] = message }
