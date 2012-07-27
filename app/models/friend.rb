06adc0a0029b54974e9cf9031cb5a50d49dd5c74
Sunny Ripert <sunny@sunfox.org>
2012-07-27 16:24:14 +0200

    Fix cache key on friends debt, now depends on user too

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -10,7 +10,9 @@ class Friend < Person
   validates :name, presence: true
 
   def debt
-    Rails.cache.fetch("#{cache_key}/debt") do
+    # FIXME should not need user.cache_key, but should "touch" all friends from
+    #       the same bill whenever a participation is modified.
+    Rails.cache.fetch("#{user.cache_key}/#{cache_key}/debt") do
       # FIXME Holy shmoly requests n+galore
       bills.to_a.sum { |bill|
         bill.debts.sum { |debt|
