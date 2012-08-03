c12931ba861c0a5e1124826e678b44286ce8f521
Sunny Ripert <sunny@sunfox.org>
2012-08-03 15:34:53 +0200

    Uses activerecord through association

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,6 +2,7 @@ class Friend < Person
   # Associations
   belongs_to :user
   has_many :participations, foreign_key: :person_id
+  has_many :bills, through: :participations
 
   # Hooks
   before_destroy :destroy_bills
@@ -20,11 +21,6 @@ class Friend < Person
         }
       }
     end
-  end
-
-  def bills
-    # has_many :bills, through: :participations, dependant: destroy
-    Bill.joins(participations: :person).where(participations: { person_id: id })
   end
 
   private
