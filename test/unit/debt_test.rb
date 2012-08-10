2687c10034f6361fa1fa6d48cc9aec431eba33d4
Sunny Ripert <sunny@sunfox.org>
2012-08-10 11:54:23 +0200

    One test for each debt method

--- test/unit/debt_test.rb
+++ test/unit/debt_test.rb
@@ -3,23 +3,38 @@ require 'test_helper'
 class DebtTest < ActiveSupport::TestCase
   context "A debt" do
     setup {
-      @oren = create :friend, name: "O-Ren"
-      @elle = create :friend, name: "Elle"
-      @debt = Debt.new(@oren.id, @elle.id, 42) # from, to, amount
+      @oren = create :friend, name: "O-Ren", id: 11111
+      @elle = create :friend, name: "Elle", id: 22222
+      @debt = Debt.new(from=11111, to=22222, amount=42)
     }
 
-    should "#from #to #from_person #to_person #amount #inspect" do
-      assert_equal @oren.id, @debt.from
-      assert_equal @elle.id, @debt.to
+    should "#from " do
+      assert_equal 11111, @debt.from
+    end
+
+    should "#to" do
+      assert_equal 22222, @debt.to
+    end
+
+    should "#from_person" do
       assert_equal "O-Ren", @debt.from_person.name
+    end
+
+    should "#to_person" do
       assert_equal "Elle", @debt.to_person.name
+    end
+
+    should "#amount" do
       assert_equal 42, @debt.amount
+    end
+
+    should "#inspect" do
       assert_equal "<Debt $42 O-Ren to Elle>", @debt.inspect
     end
 
     should "#diff_for" do
-      assert_equal +42, @debt.diff_for(@oren.id)
-      assert_equal -42, @debt.diff_for(@elle.id)
+      assert_equal +42, @debt.diff_for(11111)
+      assert_equal -42, @debt.diff_for(22222)
       assert_equal   0, @debt.diff_for(create(:friend).id)
     end
   end
