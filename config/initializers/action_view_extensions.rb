3d21ee5e9f1e3f71d50bda61ad90717865c3c1b4
Sunny Ripert <sunny@sunfox.org>
2012-08-16 15:12:34 +0200

    Added and used currency_field

--- config/initializers/action_view_extensions.rb
+++ config/initializers/action_view_extensions.rb
@@ -1,20 +1,43 @@
 # encoding: utf-8
 class ActionView::Helpers::FormBuilder
-  def number_field(attribute, options={})
-    defaults = { size: 5, type: "number" }
-    text_field(attribute, defaults.merge(options))
-  end
+  # Returns an input tag of type "number" with default attributes for a
+  # currency field.
+  #
+  # ==== Options
+  # * <tt>:unit</tt> - Sets the denomination of the currency. Set to false
+  #    to disable showing the unit.
+  # * Otherwise accepts the same options as number_field_tag.
+  #
+  # ==== Examples
+  #
+  #  f.currency_field(:amount)
+  #  => $<input type="number" min="0" step="0.001" size="5" placeholder="0" />
+  #
+  #  f.currency_field(:amount, :unit => false, :placeholder => "Amount")
+  #  => <input type="number" min="0" step="0.001" size="5" placeholder="Amount" />
+  def currency_field(method, options = {})
+    options.symbolize_keys!
 
-  def money_field(attribute, options={})
-    defaults = { min: 0, step: 0.01, data: { currency: "€" } }
-    options = defaults.merge(options)
+    defaults = {
+      min: 0,
+      step: 0.01,
+      size: 5,
+      placeholder: 0,
+      unit: true
+    }
+    options = defaults.merge!(options)
 
-    currency = ""
-    if options[:data][:currency]
-      currency = content_tag(:span, options[:data][:currency], class: "currency")
+    field = number_field(method, options)
+
+    unit = options.delete(:unit)
+    unit = I18n.translate(:'number.currency.format.unit') if unit === true
+
+    if unit
+      format = I18n.translate(:'number.currency.format.format')
+      format.gsub(/%n/, unit_tag).gsub(/%u/, unit).html_safe
+    else
+      field
     end
-
-    number_field(attribute, options) + currency
   end
 end
 
