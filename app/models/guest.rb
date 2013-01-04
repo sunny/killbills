7071802ff81ac2ae9b40e741b9d27296353fd7d4
Sunny Ripert <sunny@sunfox.org>
2013-01-04 15:21:23 +0100

    Guests can save a currency

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,17 @@
 class Guest < User
+
+  # Override devise's validator on email
+  def update_attributes(*attributes)
+    update = super(*attributes)
+    if update
+      true
+    elsif errors.size == 1 and errors[:email]
+      save(validate: false)
+    else
+      false
+    end
+  end
+
   def display_name
     "Anonymous Guest"
   end
